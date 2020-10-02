5d89af1095e0bcbe092697ab22c398b13b0c9d9a
PeteDram <me@petejames.com>
2020-10-01 20:57:07 -0700

    binary search recursive complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,6 +2,25 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    #base case - what is a sub problem and stop recursion?
+    if end >= start:
+        # guess == target
+        guess = arr[((start + end) //2)]
+        if guess == target:
+            return arr.index(guess)
+
+        #how to get to base case - a sub problem.
+        # if guess high, reduce middle by 1
+        elif guess > target:
+            #add recursive call reduce end by 1
+            return binary_search(arr, target, start, end-1)
+
+        else:
+            #add recursive all increase start by 1
+            return binary_search(arr, target, start + 1, end)
+
+    else:
+            return -1 #not found
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +30,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
